b3844e3ca3d4bd7ac65a46ff7279e8e6835f2645
Markus Bertheau <mbertheau@gmail.com>
2014-11-20 08:52:26 +0100

    Update all files generated by the Cosmos compiler

--- src/table.pl
+++ src/table.pl
@@ -4,4 +4,4 @@ table_get(Env0,_t,_i,_t2) :- get(_t,_i,_t2).
 table_set(Env0,_t,_i,_x,_t2) :- set(_t,_i,_x,_t2).
 table_toListKeys(Env0,_t,_l) :- assoc_to_keys(_t,_l).
 table_toListValues(Env0,_t,_l) :- assoc_to_values(_t,_l).
-table_env(X) :- new(X0),set(X0,"toListValues",table_toListValues,X1),set(X1,"toListKeys",table_toListKeys,X2),set(X2,"set",table_set,X3),set(X3,"get",table_get,X4),X=X4.
+table_env(X) :- new(X0),set(X0,"toListValues",table_toListValues,X1),set(X1,"toListKeys",table_toListKeys,X2),set(X2,"set",table_set,X3),set(X3,"get",table_get,X4),set(X4,"Some",table_Some,X5),set(X5,"None",table_None,X6),set(X6,"Left",table_Left,X7),set(X7,"Right",table_Right,X8),X=X8.
